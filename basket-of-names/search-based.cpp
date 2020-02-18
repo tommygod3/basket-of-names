09b46905f40f567d3a4008484bf41b761f127119
tommygod3 <tommygod1@live.com>
2020-02-18 11:51:00 +0000

    Fixed search stack overflow and far east problem, cleaned bin

--- basket-of-names/search-based.cpp
+++ basket-of-names/search-based.cpp
@@ -7,11 +7,11 @@ SearchBased::SearchBased(std::string filename)
     Name firstName = this->westInput.begin()->first;
 
     this->extendResultWest(firstName);
-    this->extendResultEast(this->eastInput.at(firstName));//TODO what if firstName on far east
+    if (!isOnEnd(firstName, this->eastInput)) this->extendResultEast(this->eastInput.at(firstName));
 
-    for (auto r : this->result)
+    for (Name name : this->result)
     {
-        std::cout << r << "\n";
+        std::cout << name << std::endl;
     }
 
 }
@@ -36,20 +36,22 @@ void SearchBased::setInputDicts(std::string filename)
 
 void SearchBased::extendResultWest(Name name)
 {
-    this->result.push_back(name);
-    if (!isOnEnd(name, this->westInput))
+    while (!isOnEnd(name, this->westInput))
     {
-        extendResultWest(this->westInput.at(name));
+        this->result.push_back(name);
+        name = this->westInput.at(name);
     }
+    this->result.push_back(name);
 }
 
 void SearchBased::extendResultEast(Name name)
 {
-    this->result.push_front(name);
-    if (!isOnEnd(name, this->eastInput))
+    while (!isOnEnd(name, this->eastInput))
     {
-        extendResultEast(this->eastInput.at(name));
+        this->result.push_front(name);
+        name = this->eastInput.at(name);
     }
+    this->result.push_front(name);
 }
 
 bool SearchBased::isOnEnd(Name name, NameDict& nameDict)
